27a195c545a13bd8c2eb14b77a7b871892a1cabc
ExVee <RosaAthenaMaes@gmail.com>
2025-08-27 15:23:43 +0200

    2

--- Assets/Scripts/Dragon_Behaviour.cs
+++ Assets/Scripts/Dragon_Behaviour.cs
@@ -10,39 +10,34 @@ public class Dragon_Behaviour : MonoBehaviour
     [SerializeField] Material sweep_2;
     [SerializeField] Material fire;
     [SerializeField] bool testturn = false;
-    Material currentmaterial;
     int attack;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         attack = 0;
-        currentmaterial = gameObject.GetComponent<Material>();
     }
 
-    float timer = 0;
-    bool timerReached = false;
     
     // Update is called once per frame
     void Update()
     {
+        timer = 0;
         if (testturn == false)
-        {
-            attack = UnityEngine.Random.Range(0, 1);
-            if (attack == 0 )
             {
-                currentmaterial = sweep_1;
+            attack = UnityEngine.Random.Range(0, 1);
+            if (attack == 0 || attack == 1)
+            {
+                Debug.Log("vuur");
+                gameObject.GetComponent<Renderer>().material = fire;
             }
             else if (attack == 1)
             {
-            
+                Debug.Log("Swipe");
+                gameObject.GetComponent<Renderer>().material = sweep_1;
+                gameObject.GetComponent<Renderer>().material = sweep_2;
             }
-            testturn = true;
-            currentmaterial = idle;
-        }
-        // stage transition
-        if (dragon_hp < 50)
-        {
-
+                //testturn = true;
+                gameObject.GetComponent<Renderer>().material = idle;
         }
     }
 }
