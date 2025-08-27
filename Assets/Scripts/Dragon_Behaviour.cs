d4e69daa7aa4174b459c9b88e28b89a22e4eb459
T301104 <geurts991@gmail.com>
2025-08-27 16:29:21 +0200

    boss fixed it

--- Assets/Scripts/Dragon_Behaviour.cs
+++ Assets/Scripts/Dragon_Behaviour.cs
@@ -10,6 +10,9 @@ public class Dragon_Behaviour : MonoBehaviour
     public bool dragonTurn = false;
     [SerializeField] public float dragon_hp = 45;
     [SerializeField] HeroBehavior heroBehavior;
+    [SerializeField] AudioClip fireAttack;
+    [SerializeField] AudioClip sweepAttack;
+    AudioSource audioSource;
     [SerializeField] Material idle;
     [SerializeField] Material sweep_1;
     [SerializeField] Material sweep_2;
@@ -18,21 +21,22 @@ public class Dragon_Behaviour : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
+        audioSource = gameObject.GetComponent<AudioSource>();
     }
 
 
     // Update is called once per frame
     void Update()
     {
+
         if (dragon_hp <= 0)
         {
-        SceneManager.LoadScene("GameWin");
+            SceneManager.LoadScene("GameWin");
         }
         if (heroBehavior.player_Turn == false && dragonTurn)
             {
-                attack = UnityEngine.Random.Range(0, 1);
-            if (attack == 0 || attack == 1)
+                attack = UnityEngine.Random.Range(0, 2);
+            if (attack == 0)
             {
                 dragonTurn = false;
                 StartCoroutine(FireAttack());
@@ -45,24 +49,28 @@ public class Dragon_Behaviour : MonoBehaviour
 
             }
                 //testturn = true;
-                gameObject.GetComponent<Renderer>().material = idle;
             }
     }
 
     private IEnumerator FireAttack()
     {
         gameObject.GetComponent<Renderer>().material = fire;
+        audioSource.PlayOneShot(fireAttack);
         yield return new WaitForSeconds(1f);
         heroBehavior.player_Turn = true;
+        gameObject.GetComponent<Renderer>().material = idle;
+
     }
     private IEnumerator SwipeAttack()
     {
 
         gameObject.GetComponent<Renderer>().material = sweep_1;
-        yield return new WaitForSeconds(1f);
+        yield return new WaitForSeconds(0.5f);
         gameObject.GetComponent<Renderer>().material = sweep_2;
-        yield return new WaitForSeconds(1f);
+        audioSource.PlayOneShot(sweepAttack);
+        yield return new WaitForSeconds(0.5f);
         heroBehavior.player_Turn = true;
+        gameObject.GetComponent<Renderer>().material = idle;
 
     }
 }
